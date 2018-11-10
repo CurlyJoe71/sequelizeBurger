cf1f928a1bcb29375e38d0ad335ef00f899e1027
Curly Joe <curlyjoe71@gmail.com>
2018-11-10 09:47:29 -0700

    Updated

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,13 @@
-const orm = require('../config/orm.js');
-
-const burger = {
-    all: function(cb) {
-        orm.selectAll(function(result) {
-            cb(result);
-        });
-    },
-    create: function(newBurger, devoured, cb) {
-        orm.insertOne(newBurger, devoured, function(result) {
-            cb(result);
-        });
-    },
-    update: function(id, devoured, cb) {
-        orm.updateOne(id, devoured, function(result) {
-            cb(result);
-        });
-    }
+moduel.exports = (sequelize, dataTypes) => {
+    const Burger = sequelize.define('burgers', {
+        burger_name: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        devoured: {
+            defaultValue: false,
+            type: BOOLEAN
+        }
+    });
+    return Burger;
 };
-
-module.exports = burger;
